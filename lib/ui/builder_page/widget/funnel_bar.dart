be20d2061bdce20ef5c6e62220b63591c4f537e7
N0d1rbe <hasanovnodir2005@gmail.com>
2024-10-12 17:59:02 +0500

    build project skeleton

--- lib/ui/builder_page/widget/funnel_bar.dart
+++ lib/ui/builder_page/widget/funnel_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_funnel/core/gen/strings.dart';
 import 'package:web_funnel/core/ui/app_colors.dart';
 import 'package:web_funnel/core/ui/widgets/custom_button.dart';
 
@@ -31,12 +32,12 @@ class FunnelBar extends StatelessWidget implements PreferredSizeWidget {
           color: AppColors.buttonColor,
           borderRadius: BorderRadius.circular(16),
         ),
-        child: const Row(
+        child: Row(
           children: [
             Icon(Icons.edit, size: 18, color: AppColors.white),
             SizedBox(width: 12),
             Text(
-              'Project name',
+              Strings.projectName,
               style: TextStyle(fontSize: 16, color: AppColors.white),
             ),
           ],
@@ -51,13 +52,13 @@ class FunnelBar extends StatelessWidget implements PreferredSizeWidget {
       child: Row(
         children: [
           CustomButton(
-            text: 'Preview',
+            text: Strings.preview,
             color: AppColors.buttonColor,
             onPressed: () {},
           ),
           const SizedBox(width: 12),
           CustomButton(
-            text: 'Publish',
+            text: Strings.publish,
             color: AppColors.blue,
             onPressed: () {},
           )
